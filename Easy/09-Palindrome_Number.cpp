3b570a2b9b8ab9950324abe877dfc01f48c1be37
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-09-04 22:12:13 +0530

    Add files via upload

--- Easy/09-Palindrome_Number.cpp
+++ Easy/09-Palindrome_Number.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) {
+            return false;
+        }
+        int originalX=x;
+        long reverseNum=0;
+        while(x!=0) {
+            int rem=x%10;
+            reverseNum=reverseNum * 10+rem;
+            x=x/10;
 
+
+        }
+        return originalX == reverseNum;
+       
+
+        if(originalX == reverseNum) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+};
